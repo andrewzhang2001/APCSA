11940ddf9a51134b03d35d34629465a083d17bdc
superaz2001 <36088609+superaz2001@users.noreply.github.com>
2018-02-02 13:58:57 -0800

    Update IntroClass.java
    v.push_back(intro changes)

--- APCSA/src/intro/IntroClass.java
+++ APCSA/src/intro/IntroClass.java
@@ -5,6 +5,7 @@ public class IntroClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		System.out.println("Hello world!\nHello World!\tHelloWorld!\rHelloWorld!\"HelloWorld!\"");
+		System.out.println("Hello github");
 	}
 
 }
