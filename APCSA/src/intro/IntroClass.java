fd59f8745cfd590a3e495b2134180aba25167d8c
zhanga3284 <zhanga3284@CA-SU-F106-18.sduhsd.lan>
2018-02-02 13:57:13 -0800

    Upload APCSA project

---
+++ APCSA/src/intro/IntroClass.java
@@ -0,0 +1,10 @@
+package intro;
+
+public class IntroClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.println("Hello world!\nHello World!\tHelloWorld!\rHelloWorld!\"HelloWorld!\"");
+	}
+
+}
---
+++ APCSA/src/intro/Lab0b.java
@@ -0,0 +1,75 @@
+package intro;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 32122;
+		int intOne = 1234567812;
+
+		long longOne = 1234567711;
+
+		//decimal variables
+		
+		float floatOne = 1267.1f;
+		double doubleOne = 233333333.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+		
+		//other integer types
+		char charOne = 'w';
+
+		//other types
+		boolean booleanOne = false;
+		String stringOne = "Zadam Heng APCSP";
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Andrew Zhang           02/01/18*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t");
+		System.out.println("*64 bit - long = "+longOne+"\t\t");
+		System.out.println("*                               *");
+		System.out.println("*        real types             *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - intOne = "+intOne+"\t\t");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t");
+		System.out.println("*                               *");
+		System.out.println("*         other types           *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+booleanOne+"\t\t*");
+		System.out.println("*stringOne = "+stringOne+"\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
